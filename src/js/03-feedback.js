a1324c46263a9eeac59aa1dde78c4517d643699e
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-08 00:39:39 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
 
+const form = document.querySelector('.feedback-form');
+const saveLocalStorage = {};
+form.addEventListener('input', onInput);
+
+
+function onInput(evt) {
+    evt.preventDefault();
+
+    if (evt.target.name === 'email' || evt.target.name === 'message') {
+        const name = evt.target.name;
+        const value = evt.target.value;
+        saveLocalStorage[name] = value;
+
+        localStorage.setItem("feedback-form-state", JSON.stringify(saveLocalStorage));
+    }
+}
