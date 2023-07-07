f35f012e2d2532f0dc8985c93fafe216c7a5a050
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-07 19:11:46 +0300

    task2 start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player'
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (evt) {
+    const currentTime = evt.seconds;
+    console.log(currentTime);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime))
+});
+
+// const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
+// console.log(savedCurrentTime);
+
+//     player.setCurrentTime(30.456).then(function (seconds) {
+//         // seconds = the actual time that the player seeked to
+//     }).catch(function (error) {
+//         switch (error.name) {
+//             case 'RangeError':
+//                 // the time was less than 0 or greater than the video’s duration
+//                 break;
+
+//             default:
+//                 // some other error occurred
+//                 break;
+//         }
+//     });
+
+
+// player.getVideoTitle().then(function (title) {
+//     console.log('title:', title);
+// });
+
+
+
+
+
 
