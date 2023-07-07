0870b6b7e3c53137ba6f6d0df2caa806a0766234
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-07 20:31:33 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,39 @@
 import Player from '@vimeo/player'
 
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector(".vimeo-player");
 const player = new Player(iframe);
 
-player.on('timeupdate', function (evt) {
+
+player.on('timeupdate', onTimeupdate);
+
+function onTimeupdate(evt) {
     const currentTime = evt.seconds;
     console.log(currentTime);
     localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime))
-});
+}
 
-// const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
-// console.log(savedCurrentTime);
 
-//     player.setCurrentTime(30.456).then(function (seconds) {
-//         // seconds = the actual time that the player seeked to
-//     }).catch(function (error) {
-//         switch (error.name) {
-//             case 'RangeError':
-//                 // the time was less than 0 or greater than the video’s duration
-//                 break;
+const savedCurrentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+console.log(typeof savedCurrentTime);
 
-//             default:
-//                 // some other error occurred
-//                 break;
-//         }
-//     });
+if (savedCurrentTime) {
+    player.setCurrentTime(parseFloat(savedCurrentTime)).then(function (seconds) {
+        player.play();
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+
+                break;
+
+            default:
+                player.play()
+                break;
+        }
+    });
+}
+
+
 
 
 // player.getVideoTitle().then(function (title) {
